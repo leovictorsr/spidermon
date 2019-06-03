6c30ab9b7556f2466c2fcb5b99e65df30c5823ce
Leonardo Rodrigues <leovictorsr@gmail.com>
2019-06-03 18:20:38 -0300

    Update project structure and include version command tests

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "spidermon = spidermon.commands.spidermon",
+            "spidermon = spidermon.commands:cli",
         ],
     },
 )
